5e9203f5f4a07c36802cedba9b17cb510caf5912
douja44 <azizdouagi94@gmail.com>
2024-05-06 13:28:13 +0100

    GRR GRR AOUU

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ class config
         if (!isset(self::$pdo)) {
             try {
                 self::$pdo = new PDO(
-                    'mysql:host=localhost;dbname=projetweb2a1',
+                    'mysql:host=localhost;dbname=projetweb2a',
                     'root',
                     '',
                     [
